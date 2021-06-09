84c578c3cb15d6e7bf176852bcea50368ed69a3e
Liu Li <nox60@hotmail.com>
2021-06-09 23:41:53 +0800

    Update TabPane.vue

--- src/views/tab/components/TabPane.vue
+++ src/views/tab/components/TabPane.vue
@@ -1,19 +1,1 @@
-<template>
-  <el-table :data="list" border fit highlight-current-row style="width: 100%">
-    <el-table-column
-      v-loading="loading"
-      align="center"
-      label="ID"
-      width="65"
-      element-loading-text="请给我点时间！"
-    >
-      <template slot-scope="scope">
-        <span>{{ scope.row.id }}</span>
-      </template>
-    </el-table-column>
 
-    <el-table-column width="180px" align="center" label="Date">
-      <template slot-scope="scope">
-        <span>{{ scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-      </template>
-    </el-table-column>
